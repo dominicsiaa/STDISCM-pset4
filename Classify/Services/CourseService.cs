525f4c755fb656933ff849edd710f9b1a522ce4c
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2025-04-02 00:23:25 +0800

    feat: Added enrolling of Courses

---
+++ Classify/Services/CourseService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Classify.Services
+{
+    public class CourseService
+    {
+        private readonly HttpClient _httpClient;
+        public CourseService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<Course>> GetAvailableCourses()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Course>>("courses");
+        }
+        public async Task<HttpResponseMessage> EnrollStudent(EnrollStudentRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync("courses/enroll", request);
+            return response.EnsureSuccessStatusCode();
+        }
+    }
+
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int Units { get; set; }
+        public int Capacity { get; set; }
+        public int InstructorId { get; set; }
+        public List<int> StudentIds { get; set; }
+    }
+
+    public class EnrollStudentRequest
+    {
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+    }
+}
